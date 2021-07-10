4964c908d08b6043290b9fb94993f65289d9ec69
djkcyl <cyl@cyllive.cn>
2021-07-11 00:29:12 +0800

    修复色图的权限控制

--- saya/Pixiv/__init__.py
+++ saya/Pixiv/__init__.py
@@ -18,13 +18,11 @@ channel = Channel.current()
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
 async def main(app: GraiaMiraiApplication, group: Group, message: MessageChain):
     
-    
-    if Config.Saya.Pixiv.Disabled:
-        return await sendmsg(app=app, group=group)
-    elif group.id in Config.Saya.Pixiv.Blacklist:
-        return await sendmsg(app=app, group=group)
-    
     saying = message.asDisplay()
     if saying in ['色图', '涩图', 'setu']:
+        if Config.Saya.Pixiv.Disabled:
+            return await sendmsg(app=app, group=group)
+        elif group.id in Config.Saya.Pixiv.Blacklist:
+            return await sendmsg(app=app, group=group)
         picid = json.loads(requests.get('http://a60.one:404').text)['pic']
         await app.sendGroupMessage(group, MessageChain.create([Image_NetworkAddress(f"http://pic.a60.one:88/{picid}.jpg")]))
