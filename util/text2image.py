c896f0b18bd0647834e5fe6b92c435910d510e83
djkcyl <cyl@cyllive.cn>
2021-08-11 22:58:39 +0800

    新增B站视频解析功能

--- util/text2image.py
+++ util/text2image.py
@@ -1,5 +1,3 @@
-import re
-
 from io import BytesIO
 from PIL import Image, ImageFont, ImageDraw
 
@@ -9,18 +7,12 @@ font_file = './font/sarasa-mono-sc-semibold.ttf'
 font = ImageFont.truetype(font_file, 18)
 
 
-async def create_image(text: str, cut=64, transparent=False):
+async def create_image(text: str, cut=64):
     imageio = BytesIO()
-    cut_str = '\n'.join(await get_cut_str(text, cut))
+    cut_str = '\n'.join(get_cut_str(text, cut))
     textx, texty = font.getsize_multiline(cut_str)
-    if transparent:
-        image = Image.new('RGBA', (textx + 40, texty + 40), (0, 0, 0, 0,))
-    else:
-        image = Image.new('RGB', (textx + 40, texty + 40), (255, 255, 255))
+    image = Image.new('RGB', (textx + 40, texty + 40), (255, 255, 255))
     draw = ImageDraw.Draw(image)
     draw.text((20, 20), cut_str, font=font, fill='#000000')
-    if transparent:
-        image.save(imageio, format="PNG")
-    else:
-        image.save(imageio, format="JPEG", quality=85)
+    image.save(imageio, format="JPEG", quality=85)
     return imageio
