1162bdfb4c5e38ed42b51cf552bb1af3763005a6
djkcyl <cyl@cyllive.cn>
2021-08-13 19:19:33 +0800

    修改了readme

--- saya/AdminMSG.py
+++ saya/AdminMSG.py
@@ -28,44 +28,6 @@ async def get_botQueue(app: GraiaMiraiApplication, member: Member, message: Mess
                 pass
 
 
-@channel.use(ListenerSchema(listening_events=[GroupMessage], inline_dispatchers=[Literature("/setnick")]))
-async def unmute(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-    if member.id in yaml_data['Basic']['Permission']['Admin']:
-        saylist = message.asDisplay().split()
-        qq = message.get(At)[0].target
-        await app.modifyMemberInfo(qq, MemberInfo(name=(saylist[2])), group)
-
-
-@channel.use(ListenerSchema(listening_events=[GroupMessage], inline_dispatchers=[Literature("/mute")]))
-async def message_revoke(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-    if member.id in yaml_data['Basic']['Permission']['Admin']:
-        mute_list = message.asDisplay().split()
-        print(mute_list)
-        qq = message.get(At)[0].target
-        time = int(mute_list[2])
-        await app.mute(group, qq, time)
-
-
-@channel.use(ListenerSchema(listening_events=[GroupMessage], inline_dispatchers=[Literature("/unmute")]))
-async def unmute(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-    if member.id in yaml_data['Basic']['Permission']['Admin']:
-        unmute_list = message.asDisplay().split()
-        print(unmute_list)
-        qq = message.get(At)[0].target
-        await app.unmute(group, qq)
-
-
-@channel.use(ListenerSchema(listening_events=[FriendMessage], inline_dispatchers=[Literature("/unmute")]))
-async def livegroup(app: GraiaMiraiApplication, friend: Friend, message: MessageChain):
-    if friend.id in yaml_data['Basic']['Permission']['Admin']:
-        say = message.asDisplay().split()
-        groupid = say[1]
-        try:
-            await app.unmute(groupid, friend.id)
-        except PermissionError:
-            await app.sendFriendMessage(friend, MessageChain.create([Plain(f"权限不足，无法取消禁言")]))
-
-
 @channel.use(ListenerSchema(listening_events=[FriendMessage], inline_dispatchers=[Literature("全员充值")]))
 async def main(app: GraiaMiraiApplication, friend: Friend, message: MessageChain):
     if friend.id == yaml_data['Basic']['Permission']['Master']:
