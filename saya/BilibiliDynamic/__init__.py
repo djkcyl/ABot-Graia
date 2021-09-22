0a866c619d52c0911281860e4c9dcc4dd129e70e
djkcyl <cyl@cyllive.cn>
2021-09-22 14:17:57 +0800

    更新哔哩哔哩直播推送

--- saya/BilibiliDynamic/__init__.py
+++ saya/BilibiliDynamic/__init__.py
@@ -25,11 +25,13 @@ from .dynamic_shot import get_dynamic_screenshot
 saya = Saya.current()
 channel = Channel.current()
 
-OFFSET = {}
+DYNAMIC_OFFSET = {}
+LIVE_STATUS = {}
 NONE = False
 
 head = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    'Referer': 'https://www.bilibili.com/'
 }
 
 if os.path.exists('./saya/BilibiliDynamic/dynamic_list.json'):
@@ -51,8 +53,15 @@ def get_group_sub(groupid):
     return num
 
 
+def get_subid_list():
+    subid_list = []
+    for subid in dynamic_list['subscription']:
+        subid_list.append(subid)
+    return subid_list
+
+
 async def add_uid(uid, groupid):
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(proxies=get_proxy(), headers=head) as client:
         r = await client.get(f"https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid={uid}")
         r = r.json()
     if "cards" in r["data"]:
@@ -64,7 +73,7 @@ async def add_uid(uid, groupid):
             if uid not in dynamic_list['subscription']:
                 dynamic_list['subscription'][uid] = []
                 last_dynid = r["data"]["cards"][0]["desc"]["dynamic_id"]
-                OFFSET[uid] = last_dynid
+                DYNAMIC_OFFSET[uid] = last_dynid
             if get_group_sub(groupid) == 8:
                 return Plain(f"每个群聊最多仅可订阅 8 个 UP")
             dynamic_list['subscription'][uid].append(groupid)
@@ -102,10 +111,24 @@ async def init(app: GraiaMiraiApplication):
     if yaml_data['Saya']['BilibiliDynamic']['Disabled']:
         return
 
-    sub_num = len(dynamic_list["subscription"])
+    subid_list = get_subid_list()
+    sub_num = len(subid_list)
+    if sub_num == 0:
+        return app.logger.info(f"[BiliBili推送] 由于未订阅任何账号，本次初始化结束")
     info_msg = [f"[BiliBili推送] 将对 {sub_num} 个账号进行监控"]
+
+    data = {"uids": subid_list}
+    async with httpx.AsyncClient(proxies=get_proxy(), headers=head) as client:
+        r = await client.post("https://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids", json=data)
+        r = r.json()
+    for uid_statu in r["data"]:
+        if r["data"][uid_statu]["live_status"] == 1:
+            LIVE_STATUS[uid_statu] = True
+        else:
+            LIVE_STATUS[uid_statu] = False
+
     i = 1
-    for up_id in dynamic_list["subscription"]:
+    for up_id in subid_list:
         for ri in range(5):
             try:
                 async with httpx.AsyncClient(proxies=get_proxy(), headers=head) as client:
@@ -113,7 +136,7 @@ async def init(app: GraiaMiraiApplication):
                     res = r.json()
                 if "cards" in res["data"]:
                     last_dynid = res["data"]["cards"][0]["desc"]["dynamic_id"]
-                    OFFSET[up_id] = last_dynid
+                    DYNAMIC_OFFSET[up_id] = last_dynid
                     up_name = res["data"]["cards"][0]["desc"]["user_profile"]["info"]["uname"]
                     if len(str(i)) == 1:
                         si = f"  {i}"
@@ -121,8 +144,12 @@ async def init(app: GraiaMiraiApplication):
                         si = f" {i}"
                     else:
                         si = i
+                    if LIVE_STATUS.get(up_id, False):
+                        live_status = "已开播"
+                    else:
+                        live_status = "未开播"
                     sub_count = len(dynamic_list["subscription"][up_id])
-                    info_msg.append(f"  ● {si}  ---->  {up_name}({up_id}) > {sub_count} > {last_dynid}")
+                    info_msg.append(f"  ● {si}  ---->  {up_name}({up_id}) > {sub_count} > {last_dynid} > 当前{live_status}")
                     i += 1
                     await asyncio.sleep(1)
                 else:
@@ -154,11 +181,63 @@ async def update_scheduled(app: GraiaMiraiApplication):
     if yaml_data['Saya']['BilibiliDynamic']['Disabled']:
         return
 
-    app.logger.info("[BiliBili推送] 正在检测动态更新")
     if not NONE:
         app.logger.info("[BiliBili推送] 初始化未完成，终止本次更新")
         return
+
     sub_list = dynamic_list["subscription"].copy()
+    subid_list = get_subid_list()
+    post_data = {"uids": subid_list}
+    app.logger.info("[BiliBili推送] 正在检测直播更新")
+    for retry in range(3):
+        try:
+            async with httpx.AsyncClient(proxies=get_proxy(), headers=head) as client:
+                r = await client.post(f"https://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids", json=post_data)
+                live_statu = r.json()
+                if r.status_code == 200:
+                    break
+        except httpx.HTTPError as e:
+            app.logger.error(f"[BiliBili推送] 直播更新失败，正在重试")
+            app.logger.error(f"{str(type(e))}\n{str(e.request)}\n{str(e)}")
+        except Exception as e:
+            app.logger.error(f"[BiliBili推送] 直播更新失败，正在重试")
+            app.logger.error(f"{str(type(e))}\n{str(e)}")
+            image = await create_image(f"{str(type(e))}\n{str(e)}", 120)
+            await app.sendFriendMessage(yaml_data['Basic']['Permission']['Master'], MessageChain.create([
+                Plain(f"BiliBili 直播检测失败 {retry + 1} 次\n"),
+                Image_UnsafeBytes(image.getvalue())
+            ]))
+    else:
+        app.logger.error(f"[BiliBili推送] 直播更新失败超过 {retry + 1} 次，已终止本次更新")
+        return await app.sendFriendMessage(yaml_data['Basic']['Permission']['Master'], MessageChain.create([
+            Plain(f"BiliBili 直播检测失败超过 {retry + 1} 次，已终止本次检测")
+        ]))
+
+    for up_id in live_statu["data"]:
+        title = live_statu["data"][up_id]["title"]
+        room_id = live_statu["data"][up_id]["room_id"]
+        room_area = live_statu["data"][up_id]["area_v2_parent_name"] + " / " + live_statu["data"][up_id]["area_v2_name"]
+        up_name = live_statu["data"][up_id]["uname"]
+
+        if live_statu["data"][up_id]["live_status"] == 1:
+            if LIVE_STATUS[up_id]:
+                continue
+            else:
+                LIVE_STATUS[up_id] = True
+                app.logger.info(f"[BiliBili推送] {up_name} 开播了 - {room_area} - {title}")
+                for groupid in sub_list[up_id]:
+                    await app.sendGroupMessage(groupid, MessageChain.create([
+                        Plain(f"本群订阅的UP {up_name}（{up_id}）在 {room_area} 开播啦 ！\n"),
+                        Plain(title),
+                        Plain(f"\nhttps://live.bilibili.com/{room_id}")
+                    ]))
+                    await asyncio.sleep(0.3)
+        else:
+            if LIVE_STATUS[up_id]:
+                app.logger.info(f"[BiliBili推送] {up_name} 已下播")
+                LIVE_STATUS[up_id] = False
+
+    app.logger.info("[BiliBili推送] 正在检测动态更新")
     for up_id in sub_list:
         for retry in range(3):
             try:
@@ -167,15 +246,10 @@ async def update_scheduled(app: GraiaMiraiApplication):
                     if r.status_code == 200:
                         break
             except httpx.HTTPError as e:
-                app.logger.error(f"[BiliBili推送] 更新失败，正在重试")
+                app.logger.error(f"[BiliBili推送] 动态更新失败，正在重试")
                 app.logger.error(f"{str(type(e))}\n{str(e.request)}\n{str(e)}")
-                # image = await create_image(f"{str(type(e))}\n{str(e.request)}\n{str(e)}", 400)
-                # await app.sendFriendMessage(yaml_data['Basic']['Permission']['Master'], MessageChain.create([
-                #     Plain(f"BiliBili 动态检测失败 {retry + 1} 次\n"),
-                #     Image_UnsafeBytes(image.getvalue())
-                # ]))
             except Exception as e:
-                app.logger.error(f"[BiliBili推送] 更新失败，正在重试")
+                app.logger.error(f"[BiliBili推送] 动态更新失败，正在重试")
                 app.logger.error(f"{str(type(e))}\n{str(e)}")
                 image = await create_image(f"{str(type(e))}\n{str(e)}", 120)
                 await app.sendFriendMessage(yaml_data['Basic']['Permission']['Master'], MessageChain.create([
@@ -183,7 +257,7 @@ async def update_scheduled(app: GraiaMiraiApplication):
                     Image_UnsafeBytes(image.getvalue())
                 ]))
         else:
-            app.logger.error(f"[BiliBili推送] 更新失败超过 {retry + 1} 次，已终止本次更新")
+            app.logger.error(f"[BiliBili推送] 动态更新失败超过 {retry + 1} 次，已终止本次更新")
             await app.sendFriendMessage(yaml_data['Basic']['Permission']['Master'], MessageChain.create([
                 Plain(f"BiliBili 动态检测失败超过 {retry + 1} 次，已终止本次检测")
             ]))
@@ -193,13 +267,13 @@ async def update_scheduled(app: GraiaMiraiApplication):
             up_name = r["data"]["cards"][0]["desc"]["user_profile"]["info"]["uname"]
             up_last_dynid = r["data"]["cards"][0]["desc"]["dynamic_id"]
             app.logger.debug(f"[BiliBili推送] 正在检测{up_name}（{up_id}）")
-            if up_last_dynid > OFFSET[up_id]:
+            if up_last_dynid > DYNAMIC_OFFSET[up_id]:
                 app.logger.info(f"[BiliBili推送] {up_name} 更新了动态 {up_last_dynid}")
-                OFFSET[up_id] = up_last_dynid
+                DYNAMIC_OFFSET[up_id] = up_last_dynid
                 shot_image = await get_dynamic_screenshot(r["data"]["cards"][0]["desc"]["dynamic_id_str"])
                 for groupid in sub_list[up_id]:
                     await app.sendGroupMessage(groupid, MessageChain.create([
-                        Plain(f"本群订阅的UP {up_name}（{up_id}）更新啦！"),
+                        Plain(f"本群订阅的UP {up_name}（{up_id}）更新动态啦！"),
                         Image_UnsafeBytes(shot_image)
                     ]))
                     await asyncio.sleep(0.3)
@@ -210,7 +284,7 @@ async def update_scheduled(app: GraiaMiraiApplication):
             await app.sendFriendMessage(yaml_data['Basic']['Permission']['Master'], MessageChain.create([
                 Plain(f"{up_id} 暂时无法监控，已从列表中移除")
             ]))
-    app.logger.info("[BiliBili推送] 本轮动态检测完成")
+    app.logger.info("[BiliBili推送] 本轮检测完成")
 
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage],
