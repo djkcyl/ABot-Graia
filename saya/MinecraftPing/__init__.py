48f5198a2a7c19fb87ec91f7b088e8981aaec2c4
djkcyl <cyl@cyllive.cn>
2021-09-27 21:51:53 +0800

    修复mcping

--- saya/MinecraftPing/__init__.py
+++ saya/MinecraftPing/__init__.py
@@ -1,14 +1,12 @@
-import re
-
-from graia.application import GraiaMiraiApplication
 from graia.saya import Saya, Channel
-from graia.saya.builtins.broadcast.schema import ListenerSchema
-from graia.application.event.messages import GroupMessage
 from graia.application.group import Group
-from graia.application.message.elements.internal import MessageChain, Plain
+from graia.application import GraiaMiraiApplication
+from graia.application.event.messages import GroupMessage
+from graia.saya.builtins.broadcast.schema import ListenerSchema
 from graia.application.message.parser.literature import Literature
+from graia.application.message.elements.internal import MessageChain, Plain
 
-from util.RestControl import rest_control
+from util.limit import group_limit_check
 from util.UserBlock import group_black_list_block
 from config import yaml_data, group_data, sendmsg
 
@@ -20,19 +18,21 @@ channel = Channel.current()
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage],
                             inline_dispatchers=[Literature("/mcping")],
-                            headless_decorators=[rest_control(), group_black_list_block()]))
-async def minecraft_ping(app: GraiaMiraiApplication, group: Group, saying: MessageChain):
+                            headless_decorators=[group_limit_check(15), group_black_list_block()]))
+async def minecraft_ping(app: GraiaMiraiApplication, group: Group, message: MessageChain):
 
     if yaml_data['Saya']['MinecraftPing']['Disabled']:
         return await sendmsg(app=app, group=group)
     elif 'MinecraftPing' in group_data[group.id]['DisabledFunc']:
         return await sendmsg(app=app, group=group)
 
-    pattern = re.compile('/mcping (.*)', re.M)
-    try:
-        say = pattern.search(saying.asDisplay()).group(1)
-        send_msg = await mcping(say)
-        # print(send_msg)
-        await app.sendGroupMessage(str(group.id), MessageChain.create(send_msg))
-    except:
+    saying = message.asDisplay().split()
+    if len(saying) == 2:
+        try:
+            send_msg = await mcping(saying[1])
+            # print(send_msg)
+            await app.sendGroupMessage(str(group.id), MessageChain.create(send_msg))
+        except:
+            await app.sendGroupMessage(str(group.id), MessageChain.create([Plain("用法：/mcping 服务器地址")]))
+    else:
         await app.sendGroupMessage(str(group.id), MessageChain.create([Plain("用法：/mcping 服务器地址")]))
