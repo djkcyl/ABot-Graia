37e151b8fb075da90a9d938a235b88b567943309
djkcyl <cyl@cyllive.cn>
2021-12-05 19:54:52 +0800

    时间解析修改为本地

--- saya/Reminder/time_parser.py
+++ saya/Reminder/time_parser.py
@@ -1,32 +1,14 @@
-import httpx
+import time
+import jionlp
 
 
 async def time_parser(timestr: str) -> str:
-    url = "http://a60.one:8010/time_parser"
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(url, params={"data": timestr})
-        data = resp.json()["data"]
-        if len(data) != 1:
-            return False
-        elif "detail" not in data[0]:
-            return False
-        elif "time" not in data[0]["detail"]:
-            return False
-        else:
-            return data[0]["detail"]["time"][0]
-
-
-# import time
-# import jionlp
-
-
-# async def time_parser(timestr: str) -> str:
-#     data = jionlp.ner.extract_time(timestr, time_base=time.time())
-#     if len(data) != 1:
-#         return False
-#     elif "detail" not in data[0]:
-#         return False
-#     elif "time" not in data[0]["detail"]:
-#         return False
-#     else:
-#         return data[0]["detail"]["time"][0]
+    data = jionlp.ner.extract_time(timestr, time_base=time.time())
+    if len(data) != 1:
+        return False
+    elif "detail" not in data[0]:
+        return False
+    elif "time" not in data[0]["detail"]:
+        return False
+    else:
+        return data[0]["detail"]["time"][0]
