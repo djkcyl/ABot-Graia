a8b3db26ff39eeaabee3a4542ff8dafc8acb487d
djkcyl <cyl@cyllive.cn>
2021-12-24 18:55:18 +0800

    赛马优化，道具逻辑准备完成

--- saya/HorseRacing/game.py
+++ saya/HorseRacing/game.py
@@ -4,6 +4,8 @@ from io import BytesIO
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
 
+from .gamedata import props, HorseStatus
+
 
 FONT_PATH = Path("./font")
 font24 = ImageFont.truetype(str(FONT_PATH.joinpath("sarasa-mono-sc-semibold.ttf")), 24)
@@ -12,11 +14,12 @@ font24 = ImageFont.truetype(str(FONT_PATH.joinpath("sarasa-mono-sc-semibold.ttf"
 def draw_game(data):
     player_count = len(data["player"])
     arena_size = (500, (player_count * 50))
-    name_size = (player_count * 26) + ((player_count - 1) * 4)
+    name_size = (player_count * 24) + ((player_count - 1) * 4)
     img_size = (arena_size[0], arena_size[1] + name_size + 60)
     name_text = "\n".join(
         [
-            f"{player['horse']} 号马：{player['name']}"
+            f"{player['horse']} 号马：{player['name']}  "
+            f"{player['status']['effect']}: {player['status']['duration']}: {player['status']['value']}"
             for _, player in data["player"].items()
         ]
     )
@@ -27,8 +30,10 @@ def draw_game(data):
         (127, 185, 36),
         (108, 177, 0),
         (127, 185, 36),
+        (108, 177, 0),
+        (127, 185, 36),
     ]
-    horse_name = ["①", "②", "③", "④", "⑤", "⑥"]
+    horse_name = ["①", "②", "③", "④", "⑤", "⑥", "⑦", "⑧"]
     image = Image.new("RGB", img_size, (255, 255, 255))
     draw = ImageDraw.Draw(image)
 
@@ -47,11 +52,12 @@ def draw_game(data):
                 fill=(80, 80, 80),
             )
         draw.text(
-            (32 + (data["player"][player]["score"] * 4.14), 32 + (50 * i)),
+            (32 + (data["player"][player]["score"] * 4.14), 30 + (50 * i)),
             horse_name[data["player"][player]["horse"] - 1],
             font=font24,
             fill=(0, 0, 0),
         )
+
     draw.line((20, 20, 480, 20), fill=(80, 80, 80))
     draw.line(
         (20, 20 + (50 * player_count), 480, 20 + (50 * player_count)), fill=(80, 80, 80)
@@ -66,8 +72,45 @@ def draw_game(data):
 
 
 def run_game(data):
+    data["round"] += 1
     for player in data["player"]:
-        data["player"][player]["score"] += random.randint(8, 18) * random.uniform(
-            0.7, 1.2
-        )
-    return data
+        basic_score = random.randint(4, 12) * random.uniform(0.7, 1.2)
+        if data["player"][player]["status"]["effect"] == HorseStatus.Death:
+            data["player"][player]["status"]["duration"] = None
+            data["player"][player]["status"]["value"] = None
+            continue
+        elif data["player"][player]["status"]["effect"] == HorseStatus.Poisoning:
+            if data["player"][player]["status"]["duration"] == 1:
+                data["player"][player]["status"]["effect"] = HorseStatus.Death
+                data["player"][player]["status"]["duration"] = None
+                data["player"][player]["status"]["value"] = None
+                continue
+            else:
+                data["player"][player]["status"]["duration"] -= 1
+                basic_score *= 0.8
+        else:
+            if (
+                data["player"][player]["status"]["effect"] == HorseStatus.Freeze
+                or data["player"][player]["status"]["effect"] == HorseStatus.Dizziness
+            ):
+                basic_score = 0
+            elif (
+                data["player"][player]["status"]["effect"] == HorseStatus.Slowness
+                or data["player"][player]["status"]["effect"] == HorseStatus.SpeedUp
+            ):
+                basic_score *= data["player"][player]["status"]["value"]
+
+            if data["player"][player]["status"]["duration"] > 0:
+                data["player"][player]["status"]["duration"] -= 1
+            if data["player"][player]["status"]["duration"] == 0:
+                data["player"][player]["status"]["effect"] = HorseStatus.Normal
+                data["player"][player]["status"]["value"] = 1
+
+        data["player"][player]["score"] += basic_score
+
+
+def throw_prop(data, target, prop):
+    effect, value, duration = props[prop]
+    data["player"][target]["status"]["effect"] = effect
+    data["player"][target]["status"]["value"] = value
+    data["player"][target]["status"]["duration"] = duration
