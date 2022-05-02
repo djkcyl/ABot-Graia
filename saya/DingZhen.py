3dd80e29279b6de7329a878c129d9e8c2b073221
djkcyl <cyl@cyllive.cn>
2022-05-02 17:26:30 +0800

    v2.15.0 add： 新增一键获取丁真图，一眼顶针！

---
+++ saya/DingZhen.py
@@ -0,0 +1,34 @@
+import httpx
+
+from graia.saya import Channel
+from graia.ariadne.model import Group
+from graia.ariadne.message.element import Image
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from graia.ariadne.message.parser.twilight import Twilight, RegexMatch
+
+from util.sendMessage import safeSendGroupMessage
+from util.control import Function, Interval, Permission
+
+
+channel = Channel.current()
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        inline_dispatchers=[Twilight([RegexMatch(r"^(一眼|来[点张])[顶丁钉][真针]|[Dd]ing[！!]?$")])],
+        decorators=[
+            Function.require("DingZhen"),
+            Permission.require(),
+            Interval.require(30),
+        ],
+    )
+)
+async def ding(group: Group):
+    async with httpx.AsyncClient() as client:
+        resp = await client.get("https://api.aya1.top/randomdj?r=0")
+        await safeSendGroupMessage(
+            group, MessageChain.create([Image(url=resp.json()["url"])])
+        )
